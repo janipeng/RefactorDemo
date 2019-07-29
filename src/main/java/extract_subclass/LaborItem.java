325e75d130b00f6de1dc57f829df5da55583176b
Jani Peng <1184360257@qq.com>
2019-07-29 21:58:41 +0800

    update LaborItem constructor

--- src/main/java/extract_subclass/LaborItem.java
+++ src/main/java/extract_subclass/LaborItem.java
@@ -1,7 +1,7 @@
 package extract_subclass;
 
 public class LaborItem extends JobItem {
-    public LaborItem(int unitPrice, int quantity, Employee employee, boolean isLabor) {
-        super(unitPrice, quantity, employee, isLabor);
+    public LaborItem(int quantity, Employee employee) {
+        super(0, quantity, employee, true);
     }
 }
