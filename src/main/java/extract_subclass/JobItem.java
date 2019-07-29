7baa60e7a24cc246205dfda8779e941b6bbe332d
Jani Peng <1184360257@qq.com>
2019-07-29 21:55:53 +0800

    create new constructor

--- src/main/java/extract_subclass/JobItem.java
+++ src/main/java/extract_subclass/JobItem.java
@@ -6,11 +6,16 @@ public class JobItem {
     private Employee employee;
     private boolean isLabor;
 
-    public JobItem(int unitPrice, int quantity, Employee employee, boolean isLabor) {
+    protected JobItem(int unitPrice, int quantity, Employee employee, boolean isLabor) {
         this.unitPrice = unitPrice;
         this.quantity = quantity;
         this.employee = employee;
         this.isLabor = isLabor;
+    }
+
+    public JobItem(int unitPrice, int quantity) {
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
     }
 
     public int getTotalPrice(){
