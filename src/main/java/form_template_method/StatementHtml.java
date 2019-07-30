3c8b91a322b03b1ab13f0a07a72231c6a8a4b402
Jani Peng <1184360257@qq.com>
2019-07-30 21:02:59 +0800

    extract method

--- src/main/java/form_template_method/StatementHtml.java
+++ src/main/java/form_template_method/StatementHtml.java
@@ -4,16 +4,28 @@ import java.util.Enumeration;
 
 public class StatementHtml {
     public static String value(Customer customer) {
+        String result = headerString(customer);
         Enumeration rentals = customer.getRentals().elements();
-        String result = "<H1>Rentals for <EM>" + customer.getName() + "</EM></H1><P>\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+            result += eachRentalString(each);
         }
-        //add footer lines
-        result += "<P>You owe<EM>" + String.valueOf(customer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + String.valueOf(customer.getTotalFrequentRenterPoints()) +
-                "</EM> frequent renter points<P>";
+        result += footerString(customer);
         return result;
+    }
+
+    public static String footerString(Customer customer) {
+        String footer = "<P>You owe<EM>" + String.valueOf(customer.getTotalCharge()) + "</EM><P>\n";
+        footer += "On this rental you earned <EM>" + String.valueOf(customer.getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return footer;
+    }
+
+    public static String eachRentalString(Rental each) {
+        return each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+    }
+
+    public static String headerString(Customer customer) {
+        return "<H1>Rentals for <EM>" + customer.getName() + "</EM></H1><P>\n";
     }
 }
--- src/main/java/form_template_method/StatementText.java
+++ src/main/java/form_template_method/StatementText.java
@@ -4,17 +4,29 @@ import java.util.Enumeration;
 
 public class StatementText {
     public static String value(Customer customer) {
+        String result = headerString(customer);
         Enumeration rentals = customer.getRentals().elements();
-        String result = "Rental Record for " + customer.getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            result += eachRentalString(each);
         }
 
-        //add footer lines
-        result += "Amount owed is " + String.valueOf(customer.getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(customer.getTotalFrequentRenterPoints()) + " frequent renter points";
+        result += footerString(customer);
         return result;
+    }
+
+    public static String footerString(Customer customer) {
+        String footer = "Amount owed is " + String.valueOf(customer.getTotalCharge()) + "\n";
+        footer += "You earned " + String.valueOf(customer.getTotalFrequentRenterPoints()) + " frequent renter points";
+        return footer;
+    }
+
+    public static String eachRentalString(Rental each) {
+        return "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+    }
+
+    public static String headerString(Customer customer) {
+        return "Rental Record for " + customer.getName() + "\n";
     }
 }
