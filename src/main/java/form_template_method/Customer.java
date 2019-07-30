0a9b2e3a6c7c139369969d381b1f93b6b4cf7641
Jani Peng <1184360257@qq.com>
2019-07-30 20:51:18 +0800

    move statment html

--- src/main/java/form_template_method/Customer.java
+++ src/main/java/form_template_method/Customer.java
@@ -28,17 +28,7 @@ public class Customer {
     }
 
     public String htmlStatement() {
-        Enumeration rentals = this.rentals.elements();
-        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-        //add footer lines
-        result += "<P>You owe<EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) +
-                "</EM> frequent renter points<P>";
-        return result;
+        return StatementHtml.value(this);
     }
 
     double getTotalCharge() {
